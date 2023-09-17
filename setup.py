4297416dbea94c1554897821ae435a396edf88a0
PlayerG9 <g9yout@gmail.com>
2023-09-17 11:51:45 +0200

    small changes to the setup.py script

--- setup.py
+++ setup.py
@@ -9,18 +9,16 @@ import os.path as p
 
 HERE = p.abspath(p.dirname(__file__))
 
-with open(p.join(HERE, 'README.md'), 'r', encoding='utf-8') as readme:
-    long_description = readme.read()
 
 setup(
     name='password-library',
     version=__version__,
     description=__description__,
-    long_description=long_description,
+    long_description=open(p.join(HERE, 'README.md'), 'r', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/PlayerG9/passwordlib-py/",
     author=__author__,
     license=__license__,
+    url="https://github.com/PlayerG9/passwordlib-py/",
     project_urls={
         "Author Github": "https://github.com/PlayerG9",
         "Homepage": "https://github.com/PlayerG9/passwordlib-py/",
