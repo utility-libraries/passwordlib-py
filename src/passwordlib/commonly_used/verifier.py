1d826f1a70d003af1f8f01d227d7446c20f12fed
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-10-11 20:51:03 +0200

    rewritten verifier.py

--- src/passwordlib/commonly_used/verifier.py
+++ src/passwordlib/commonly_used/verifier.py
@@ -4,24 +4,35 @@ r"""
 
 """
 import os.path as p
+from importlib import resources
 
 
 COMMON_PASSWORDS = set()
 
 
-def init():
+def init(force: bool = False, packaged: bool = False) -> None:
     r"""
     load the password-list into the memory
 
     (normally you don't have to call this method yourself)
+
+    :param force: whether to clear the cache and read the passwords again
+    :param packaged: whether this application was packaged (e.g. zipapp) and normal reading is not possible
     """
     global COMMON_PASSWORDS
-    if len(COMMON_PASSWORDS):
-        return
-    password_file = p.join(p.dirname(__file__), 'password-list.txt')
-    with open(password_file, 'r', encoding='utf-8') as file:
-        for password in file:
-            COMMON_PASSWORDS.add(password.rstrip())
+    if not force and len(COMMON_PASSWORDS): return
+    'password-list.txt'
+    COMMON_PASSWORDS.clear()
+
+    if packaged:
+        with resources.open_text('passwordlib.commonly_used', 'password-list.txt', encoding='utf-8') as file:
+            for password in file:
+                COMMON_PASSWORDS.add(password.rstrip())
+    else:
+        password_file = p.join(p.dirname(__file__), 'password-list.txt')
+        with open(password_file, 'r', encoding='utf-8') as file:
+            for password in file:
+                COMMON_PASSWORDS.add(password.rstrip())
 
 
 def is_commonly_used(password: str) -> bool:
