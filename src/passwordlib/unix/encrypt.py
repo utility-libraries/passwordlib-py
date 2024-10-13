437569dbf0ec03b2a4b46a5de8e7002d2359c425
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-10-13 17:16:21 +0200

    code reformat

--- src/passwordlib/unix/encrypt.py
+++ src/passwordlib/unix/encrypt.py
@@ -12,10 +12,11 @@ __all__ = ['encrypt_bcrypt', 'encrypt_sha1']
 BCRYPT_PREFIX: t.TypeAlias = t.Union[t.Literal[b"2a"], t.Literal[b"2b"]]
 
 
-def encrypt_bcrypt(password: t.AnyStr, rounds: int = 12, prefix: BCRYPT_PREFIX = "2b") -> str:
+def encrypt_bcrypt(password: t.AnyStr, salt: str = None, rounds: int = 12, prefix: BCRYPT_PREFIX = "2b") -> str:
     import bcrypt
     password = fn.get_password_bytes(password)
-    return bcrypt.hashpw(password, bcrypt.gensalt(rounds=rounds, prefix=prefix)).decode()
+    salt = salt or bcrypt.gensalt(rounds=rounds, prefix=prefix)
+    return bcrypt.hashpw(password, salt=salt).decode()
 
 
 def encrypt_sha1(password: t.AnyStr) -> str:
