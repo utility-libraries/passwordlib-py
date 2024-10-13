2682502b3aded8fe616b4d059107f7643144af83
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-10-13 11:41:00 +0200

    code reformat for load_bcrypt

--- src/passwordlib/unix/loading.py
+++ src/passwordlib/unix/loading.py
@@ -10,15 +10,17 @@ __all__ = ['load_bcrypt', 'load_sha1']
 
 
 def load_bcrypt(dump: str) -> LoadedTuple:
-    if dump[0] != "$": raise PWLibSyntaxError()
-    prefix, sep, rest = dump[1:].partition("$")
-    if sep is None: raise PWLibSyntaxError()
+    parts = dump.split("$")
+    if len(parts) == 4: raise PWLibSyntaxError("bad format")
+
+    void, prefix, rounds, salt_and_hash = parts
+    if void: raise PWLibSyntaxError("dump does not start with '$'")
     if prefix not in {"2", "2a", "2b", "2x", "2y"}: raise PWLibBadPrefixError("unknown prefix")
-    rounds, sep, rest = rest.partition("$")
-    if sep is None: raise PWLibSyntaxError()
+    if not rounds.isdigit(): raise PWLibSyntaxError("rounds are not an integer")
+
     iterations = int(rounds)
-    salt = rest[:22].encode()  # note: salt is in a base-64 encoded format
-    hashed = rest[22:].encode()
+    salt = salt_and_hash[:22].encode()  # note: salt is in a base-64 encoded format
+    hashed = salt_and_hash[22:].encode()
     return LoadedTuple(algorithm="bcrypt", iterations=iterations, salt=salt, hashed=hashed)
 
 
