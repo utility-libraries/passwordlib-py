65251eddad2e96efbc16ea379bd41571062e7d23
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-10-13 11:33:37 +0200

    added bcrypt

--- src/passwordlib/unix/encrypt.py
+++ src/passwordlib/unix/encrypt.py
@@ -2,13 +2,25 @@
 r"""
 
 """
+import typing as t
+from ..core import functions as fn
 
 
 __all__ = ['encrypt_sha1']
 
 
-def encrypt_sha1(password: str):
+BCRYPT_PREFIX: t.TypeAlias = t.Union[t.Literal[b"2a"], t.Literal[b"2b"]]
+
+
+def encrypt_bcrypt(password: t.AnyStr, rounds: int = 12, prefix: BCRYPT_PREFIX = "2b") -> str:
+    import bcrypt
+    password = fn.get_password_bytes(password)
+    return bcrypt.hashpw(password, bcrypt.gensalt(rounds=rounds, prefix=prefix)).decode()
+
+
+def encrypt_sha1(password: t.AnyStr) -> str:
     from hashlib import sha1
     from base64 import b64encode
-    hashed = sha1(password.encode()).digest()
+    password = fn.get_password_bytes(password)
+    hashed = sha1(password).digest()
     return "{SHA1}" + b64encode(hashed).decode()
--- src/passwordlib/unix/loading.py
+++ src/passwordlib/unix/loading.py
@@ -3,15 +3,28 @@ r"""
 
 """
 from ..core import LoadedTuple
+from ..exceptions import *
 
 
 __all__ = ['load_sha1']
 
 
+def load_bcrypt(dump: str) -> LoadedTuple:
+    if dump[0] != "$": raise PWLibSyntaxError()
+    prefix, sep, rest = dump[1:].partition("$")
+    if sep is None: raise PWLibSyntaxError()
+    if prefix not in {"2", "2a", "2b", "2x", "2y"}: raise PWLibBadPrefixError("unknown prefix")
+    rounds, sep, rest = rest.partition("$")
+    if sep is None: raise PWLibSyntaxError()
+    iterations = int(rounds)
+    salt = rest[:22].encode()  # note: salt is in a base-64 encoded format
+    hashed = rest[22:].encode()
+    return LoadedTuple(algorithm="bcrypt", iterations=iterations, salt=salt, hashed=hashed)
+
+
 def load_sha1(dump: str) -> LoadedTuple:
     from base64 import b64decode
-    if not dump.startswith("{SHA}"):
-        raise ValueError("Invalid dump format")
+    if not dump.startswith("{SHA}"): raise PWLibBadPrefixError("Invalid dump format")
     b64encoded = dump[6:]
     hashed = b64decode(b64encoded)
     return LoadedTuple(algorithm="sha1", iterations=-1, salt=b'', hashed=hashed)
