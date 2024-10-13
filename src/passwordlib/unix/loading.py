8633bc6189083008bd5b27eaf8782c76bf752c1d
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-10-13 17:16:43 +0200

    implemented unix-loading for sha256

--- src/passwordlib/unix/loading.py
+++ src/passwordlib/unix/loading.py
@@ -24,6 +24,27 @@ def load_bcrypt(dump: str) -> LoadedTuple:
     return LoadedTuple(algorithm="bcrypt", iterations=iterations, salt=salt, hashed=hashed)
 
 
+def load_sha256(dump: str) -> LoadedTuple:
+    parts = dump.split("$")
+    n_parts = len(parts)
+    if n_parts != 4 and n_parts != 5: raise PWLibSyntaxError("bad format")
+
+    if n_parts == 4:
+        void, prefix, salt, checksum = parts
+        rounds = "rounds=5000"
+    else:
+        void, prefix, rounds, salt, checksum = parts
+    if void: raise PWLibSyntaxError("dump does not start with '$'")
+    if prefix != "5": raise PWLibSyntaxError("unknown prefix")
+    if not rounds.startswith("rounds=") or not rounds[7:].isdigit(): raise PWLibSyntaxError("bad rounds")
+
+    iterations = int(rounds[7:])
+    salt = salt.encode()
+    hashed = checksum.encode()
+
+    return LoadedTuple(algorithm="sha256", iterations=iterations, salt=salt, hashed=hashed)
+
+
 def load_sha1(dump: str) -> LoadedTuple:
     from base64 import b64decode
     if not dump.startswith("{SHA}"): raise PWLibBadPrefixError("Invalid dump format")
